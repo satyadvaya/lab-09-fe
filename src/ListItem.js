c5f50726ea2710a757394306dbe8a2b5edcc2b98
Alan Willoughby <satyadvaya@gmail.com>
2020-05-11 08:37:46 -0700

    lab update

--- src/ListItem.js
+++ src/ListItem.js
@@ -4,7 +4,10 @@ export default class ListItem extends Component {
     render() {
         return (
             <div>
-                <h3>{this.props.planet.name}</h3>
+                <h1>{this.props.planet.name}</h1>
+                <h3>Moons: {this.props.planet.moons}</h3>
+                <h3>Rings: {String(this.props.planet.rings)}</h3>
+                <h3>Type: {this.props.planet.type}</h3>
             </div>
         )
     }
